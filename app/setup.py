3df4fba58954a078c4e5a676d9c7f740d32d1a33
Luiz Quirino <luizfpq@gmail.com>
2021-03-24 22:47:50 -0300

    add version 0.0.2

--- app/setup.py
+++ app/setup.py
@@ -7,18 +7,20 @@ import requests
 __author__ = "Luiz Quirino"
 __copyright__ = "Copyleft 2021, Solar System"
 __license__ = "GPL"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Luiz Quirino"
 __email__ = "luizfpq@gmail.com"
 __status__ = "Testing"
 
 
-depends = [
-    'psutil',
-    'platform',
-    'py-cpuinfo'
-]
+def install():
+    depends = [
+        'psutil',
+        'py-cpuinfo',
+        'requests'
+    ]
 
-for item in depends:
-    cmd = "pip install {}".format(item)
-    os.system(cmd)
+
+    for item in depends:
+        cmd = "pip install {}".format(item)
+        os.system(cmd)
--- run.py
+++ run.py
@@ -5,16 +5,21 @@
 __author__      = "Luiz Quirino"
 __copyright__   = "Copyleft 2021, Solar System"
 __license__ = "GPL"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Luiz Quirino"
 __email__ = "luizfpq@gmail.com"
 __status__ = "Testing"
 
 import requests
-import os
+import os,sys
 from app.update import update
 from app.main import *
+from app.setup import install
 
 path=(os.path.dirname(__file__))
+
+if ('install' in sys.argv[1:]) :
+    install()
+
 update(path)
 start()
